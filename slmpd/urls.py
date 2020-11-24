baa978aefd22dc9b0a7e6eff7edd76fcd131e046
phecht <peteehecht@gmail.com>
2020-11-23 21:15:07 -0700

    Added basic detail view of crime

--- slmpd/urls.py
+++ slmpd/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import HomePageViewSlmpd, AboutPageViewSlmpd, CrimeReportPageViewSlmpd
+from .views import CrimeDetailPageViewSlmpd
 
 urlpatterns = [
     path('', HomePageViewSlmpd.as_view(), name='slmpdhome'),
     path('about/', AboutPageViewSlmpd.as_view(), name='slmpdabout'),
-    path('crime/', CrimeReportPageViewSlmpd.as_view(), name='slmpdcrime'),    
+    path('crime/', CrimeReportPageViewSlmpd.as_view(), name='slmpdcrime'), 
+    path('crime/detail/<int:pk>/', CrimeDetailPageViewSlmpd.as_view(), name='crime_detail'),    
 ]
--- slmpd/views.py
+++ slmpd/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
+from django.views.generic import DetailView 
 from .models import crime_reports
 
 
@@ -17,3 +18,7 @@ class CrimeReportPageViewSlmpd(ListView):
     context_object_name = 'crimereport'
     model = crime_reports
 
+class CrimeDetailPageViewSlmpd(DetailView):
+    template_name = 'slmpd/crimedetail.html'
+    context_object_name = 'crimedetail'
+    model = crime_reports
